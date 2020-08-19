5f439ee11ad487228cf5a696516dec34326f9b22
EricSeokgon <leesk55@gmail.com>
2020-08-19 10:53:55 +0900

    Initial commit

---
+++ src/main/java/com/bals/kakaologin/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.bals.kakaologin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "index";
+    }
+}
