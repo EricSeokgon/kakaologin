6436fceaac89aa8b062cc6f212f5515b849e59f4
EricSeokgon <leesk55@gmail.com>
2020-08-20 14:55:41 +0900

    카카오 SNS 연동 rest API

--- src/main/java/com/bals/kakaologin/controller/HomeController.java
+++ src/main/java/com/bals/kakaologin/controller/HomeController.java
@@ -18,57 +18,22 @@ public class HomeController {
     @Autowired
     private KakaoAPI kakao;
 
-    private kakao_restapi kakao_restapi = new kakao_restapi();
-
-    @RequestMapping(value = "/oauth", produces = "application/json", method = { RequestMethod.GET, RequestMethod.POST })
-    public String kakaoLogin(@RequestParam("code") String code) {
-        //카카오 홈페이지에서 받은 결과 코드
-        System.out.println(code);
-        //카카오 rest api 객체 선언
-        kakao_restapi kr = new kakao_restapi();
-        //결과값을 node에 담아줌
-        JsonNode node = kr.getAccessToken(code);
-        //결과값 출력
-        System.out.println(node);
-        //노드 안에 있는 access_token값을 꺼내 문자열로 변환
-        String token = node.get("access_token").toString();
-        System.out.println(token);
-        return "home";
-    }
-
-    @RequestMapping(value = "/oauth", produces = "application/json")
-    public String kakaoLogin(@RequestParam("code") String code, Model model, HttpSession session) {
-        System.out.println("로그인 할때 임시 코드값");
-        //카카오 홈페이지에서 받은 결과 코드
-        System.out.println(code);
-        System.out.println("로그인 후 결과값");
-
-        //카카오 rest api 객체 선언
-        kakao_restapi kr = new kakao_restapi();
-        //결과값을 node에 담아줌
-        JsonNode node = kr.getAccessToken(code);
-        //결과값 출력
-        System.out.println(node);
-        //노드 안에 있는 access_token값을 꺼내 문자열로 변환
-        String token = node.get("access_token").toString();
-        //세션에 담아준다.
-        session.setAttribute("token", token);
-        return "logininfo";
-    }
-
-    @RequestMapping(value = "/logout", produces = "application/json")
-    public String Logout(HttpSession session) {
-        //kakao restapi 객체 선언
-        kakao_restapi kr = new kakao_restapi();
-        //노드에 로그아웃한 결과값음 담아줌 매개변수는 세션에 잇는 token을 가져와 문자열로 변환
-        JsonNode node = kr.Logout(session.getAttribute("token").toString());
-        //결과 값 출력
-        System.out.println("로그인 후 반환되는 아이디 : " + node.get("id"));
-        return "redirect:/";
-    }
-
     @RequestMapping(value = "/")
     public String index() {
+        return "index";
+    }
+    @RequestMapping(value = "/oauth", produces = "application/json")
+    public String kakaoLogin(@RequestParam("code") String code, Model model, HttpSession session) {
+        String access_Token = kakao.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+
+        //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
+
         return "index";
     }
 
